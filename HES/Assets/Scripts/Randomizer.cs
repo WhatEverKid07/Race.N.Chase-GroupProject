79d8b50288f498e61eab90a2806d3d099a1490f4
WhatEverKid07 <eugeniosanc22@outlook.com>
2024-01-09 11:51:03 +0000

    Random.Range

--- HES/Assets/Scripts/Randomizer.cs
+++ HES/Assets/Scripts/Randomizer.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int randNum;
+    public GameObject Barricade, BarricadeWRamp;
+
     void Start()
     {
-        
+        randNum = Random.Range(0, 3);
+        if(randNum == 0) 
+        {
+            Barricade.SetActive(true);
+            Debug.Log("Barricade");
+        }
+
+        if (randNum == 1)
+        {
+            BarricadeWRamp.SetActive(true);
+            Debug.Log("BarricadeWRamp");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
