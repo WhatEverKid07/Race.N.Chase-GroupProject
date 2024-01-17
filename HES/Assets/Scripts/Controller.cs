e492c01036351724dbee8273cb9d378644252306
WhatEverKid07 <eugeniosanc22@outlook.com>
2024-01-17 12:11:00 +0000

    Car Controller

--- HES/Assets/Scripts/Controller.cs
+++ HES/Assets/Scripts/Controller.cs
@@ -6,7 +6,7 @@ public class Controller : MonoBehaviour
 {
     public WheelCollider[] wheels = new WheelCollider[4];
     public GameObject[] wheelMesh = new GameObject[4];
-    public float torque = 200;
+    public float torque = 5000f;
     public float steeringMax = 4;
     // Start is called before the first frame update
     void Start()
